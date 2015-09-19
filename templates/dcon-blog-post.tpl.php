3c8070061094e68308b6ae7c99d06150dd2e3f52
Jose Jiménez <jose.jimenez@wunderkraut.com>
2015-09-19 15:50:07 +0200

    Code standars

--- templates/dcon-blog-post.tpl.php
+++ templates/dcon-blog-post.tpl.php
@@ -7,8 +7,8 @@
 
 <h2><?php print $title ?></h2>
 <p><?php print $body ?></p>
-<h3>Comentarios</h3>
+<h3>Comments</h3>
 <?php foreach ($comments as $comment): ?>
-<h4><?php print $comment->name ?></h4>
-<p><?php print $comment->body ?></p>
+  <h4><?php print $comment->name ?></h4>
+  <p><?php print $comment->body ?></p>
 <?php endforeach; ?>
